70a4659d5093c6954141a814e3a2e82da785c6a3
Billy Phillips <36427489+Good-Will-Coding@users.noreply.github.com>
2019-10-01 20:57:12 +0100

    Update Bird.java

--- src/Bird.java
+++ src/Bird.java
@@ -3,9 +3,26 @@ import java.util.*;
 public class Bird {
     private String name;
     private String latinName;
+    private int count;
 
     public Bird(String name, String latinName) {
         this.name = name;
         this.latinName = latinName;
+    }
+
+    public String getName() {
+      return this.name;
+    }
+
+    public String getLatinName() {
+      return this.latinName;
+    }
+
+    public void increaseBirdCounter() {
+      count++;
+    }
+
+    public String toString() {
+      return this.name + "(" + this.latinName + "):" + this.count + " observations";
     }
 }
